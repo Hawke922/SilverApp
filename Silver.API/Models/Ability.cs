f9f3389d5b78713f03921dcdfee5ae109efa3c81
Hawke922 <tom.klucar@gmail.com>
2020-03-27 14:59:51 +0100

    pre-ability overhaul

---
+++ Silver.API/Models/Ability.cs
@@ -0,0 +1,12 @@
+namespace Silver.API.Models
+{
+    public class Ability
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public int BaseDamage { get; set; }
+        
+    }
+}
