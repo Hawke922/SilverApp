5899b7a1f7ae9ff8b7377d3747d25db0e96e9b8e
Hawke922 <tom.klucar@gmail.com>
2020-04-05 12:16:53 +0200

    Pre-Combat Overhaul

--- Silver-SPA/src/app/combat/combat.component.ts
+++ Silver-SPA/src/app/combat/combat.component.ts
@@ -83,47 +83,54 @@ export class CombatComponent implements OnInit, OnDestroy {
       this.enemyOffense = this.data.enemy.abilities[1];
     }
     this.enemyDefense = this.data.enemy.abilities[2];
-    console.log(this.enemyDefense);
-    console.log(this.enemyOffense);
+  }
+
+  enemyPickProto() {
+    // defense - will pick according to hero attack last turn, need to implement buffer for last actions
+
+    // offense - enemy can have ultimate ability on cooldown, will use it when it goes off
+  }
+
+  abilityLog(ability) {
+    // abilityList = []
+    // abilityList[0] = div 1, abilityList[1] = div 2, etc;
+    // ability.push => abiliyList
+    // abilityList[i] if i > 5 => abilityList remove last item
   }
 
   processHero() {
+    const damage = this.offense.baseDamage + this.data.character.strongAttack;
+
     if (this.offense.typeId === 1) {
-      const damage = this.offense.baseDamage + this.data.character.strongAttack;
       if (this.enemyDefense.typeId === 1) {
         this.heroDamage = damage * 0.25;
-        this.blockedDamage = damage - this.heroDamage;
       } else if (this.enemyDefense.typeId === 2) {
         this.heroDamage = damage * 0.75;
-        this.blockedDamage = damage - this.heroDamage;
       } else {
         this.heroDamage = damage;
       }
     } else if (this.offense.typeId === 2) {
-      const damage = this.offense.baseDamage + this.data.character.fastAttack;
       if (this.enemyDefense.typeId === 1) {
         this.heroDamage = damage;
       } else if (this.enemyDefense.typeId === 2) {
         this.heroDamage = damage * 0.25;
-        this.blockedDamage = damage - this.heroDamage;
       } else {
         this.heroDamage = damage * 0.75;
-        this.blockedDamage = damage - this.heroDamage;
       }
     } else {
-      const damage = this.offense.baseDamage + this.data.character.specialAttack;
       if (this.enemyDefense.typeId === 1) {
         this.heroDamage = damage * 0.75;
-        this.blockedDamage = damage - this.heroDamage;
       } else if (this.enemyDefense.typeId === 2) {
         this.heroDamage = damage;
       } else {
         this.heroDamage = damage * 0.25;
-        this.blockedDamage = damage - this.heroDamage;
       }
     }
+
+    this.blockedDamage = damage - this.heroDamage;
     this.enemyHealth -= this.heroDamage;
     this.enemyHealthBarValue = this.percentage(this.data.enemy.hp, this.enemyHealth);
+
     if (Math.sign(this.enemyHealthBarValue) === -1 || 0 || -0) {
       this.enemyHealthBarValue = 0;
       this.enemyHealth = 0;
@@ -132,42 +139,38 @@ export class CombatComponent implements OnInit, OnDestroy {
   }
 
   processEnemy() {
+    const damage = this.enemyOffense.baseDamage + this.data.enemy.strongAttack;
+
     if (this.enemyOffense.typeId === 1) {
-      const damage = this.enemyOffense.baseDamage + this.data.enemy.strongAttack;
       if (this.defense.typeId === 1) {
         this.enemyDamage = damage * 0.25;
-        this.blockedDamage = damage - this.enemyDamage;
       } else if (this.defense.typeId === 2) {
         this.enemyDamage = damage * 0.75;
-        this.blockedDamage = damage - this.enemyDamage;
       } else {
         this.enemyDamage = damage;
       }
     } else if (this.enemyOffense.typeId === 2) {
-      const damage = this.enemyOffense.baseDamage + this.data.enemy.fastAttack;
       if (this.defense.typeId === 1) {
         this.enemyDamage = damage;
       } else if (this.defense.typeId === 2) {
         this.enemyDamage = damage * 0.25;
-        this.blockedDamage = damage - this.enemyDamage;
       } else {
         this.enemyDamage = damage * 0.75;
-        this.blockedDamage = damage - this.enemyDamage;
       }
     } else {
-      const damage = this.enemyOffense.baseDamage + this.data.enemy.specialAttack;
       if (this.defense.typeId === 1) {
         this.enemyDamage = damage * 0.75;
-        this.blockedDamage = damage - this.enemyDamage;
       } else if (this.defense.typeId === 2) {
         this.enemyDamage = damage;
       } else {
         this.enemyDamage = damage * 0.25;
-        this.blockedDamage = damage - this.enemyDamage;
       }
     }
+
+    this.blockedDamage = damage - this.enemyDamage;
     this.heroHealth -= this.enemyDamage;
     this.heroHealthBarValue = this.percentage(this.data.character.hp, this.heroHealth);
+
     if (Math.sign(this.heroHealthBarValue) === -1 || 0 || -0) {
       this.heroHealthBarValue = 0;
       this.heroHealth = 0;
@@ -252,14 +255,5 @@ export class CombatComponent implements OnInit, OnDestroy {
 
   percentage(oldValue, newValue) {
   return (newValue / oldValue) * 100;
-  }
-
-  endEncounter() {
-    this.router.navigate(['/charselect']);
-  }
-
-  print() {
-    console.log(this.offense);
-    console.log(this.defense);
   }
 }
